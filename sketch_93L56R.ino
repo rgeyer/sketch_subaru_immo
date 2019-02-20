0baaa6668d5ef017754080d5794575e62b89b7c2
Ryan Geyer <me@ryangeyer.com>
2019-02-19 17:08:19 -0800

    Switch to COBS packets for serial communication. Remove fixed size in-memory buffer for EEPROM contents

--- sketch_93L56R.ino
+++ sketch_93L56R.ino
@@ -1,4 +1,5 @@
-# include <SPI.h>
+#include <PacketSerial.h>
+#include <SPI.h>
 
 #if defined(TEENSYDUINO)
   #if defined(__AVR_ATmega32U4__)
@@ -12,28 +13,53 @@
   #error
 #endif
 
+uint8_t RESET_REQ_PACKET[1] = {0};
+uint8_t RESET_ACK_PACKET[1] = {128};
+uint8_t READ_ACK_PACKET[1] = {129};
+uint8_t WRITE_ACK_PACKET[1] = {130};
+
 const int slave_pin = 10;
-String command = "00";
-bool execute_command = false;
-int eeprom_idx = 0;
-byte eeprom_data[256];
+PacketSerial pSer;
 
 SPISettings spiSettings(450000, MSBFIRST, SPI_MODE0);
 
 void reset() {
-  command = "00";
-  execute_command = false;
-  eeprom_idx = 0;
-  memset(eeprom_data, 0, 256);
+  // TODO: Is there really any need for this? Nothing to reset, really...
+  pSer.send(RESET_ACK_PACKET, 1);
+}
+
+void onSerPacketReceived(const uint8_t* buffer, size_t size) {
+  unsigned int addr = 0;
+  unsigned int len = 256;
+  switch (buffer[0]) {
+    case 0x00:
+      reset();
+      break;
+    case 0x01:
+      if (size >= 3) {
+        addr = (int)(buffer[1] << 8) | (int)(buffer[2]);
+      }
+      if (size >= 5) {
+        len = (int)(buffer[3] << 8) | (int)(buffer[4]);
+      }
+      eeprom_read(addr, len);
+      break;
+    case 0x02:
+      addr = (int)(buffer[1] << 8) | (int)(buffer[2]);
+      eeprom_write(addr, buffer);
+      break;
+  }
 }
 
 void setup() {
   Serial.begin(9600);
+  pSer.setStream(&Serial);
+  pSer.setPacketHandler(&onSerPacketReceived);
   pinMode(slave_pin, OUTPUT);
   SPI.begin();
 }
 
-void eeprom_write() {
+void eeprom_write(unsigned int addr, uint8_t* buffer) {
   // Write enable first!
   SPI.beginTransaction(spiSettings);
   digitalWrite(slave_pin, HIGH);
@@ -41,46 +67,38 @@ void eeprom_write() {
   SPI.transfer(0xc0);
   digitalWrite(slave_pin, LOW);
 
-  // Write 256 bytes
-  for (int i=0; i <= 127; i++) {
-    byte one = eeprom_data[i * 2];
-    byte two = eeprom_data[i * 2 + 1];
-    digitalWrite(slave_pin, HIGH);
-    SPI.transfer(0x05); // Write command
-    SPI.transfer(i); // Address 0
-    SPI.transfer(one);
-    SPI.transfer(two);
-    digitalWrite(slave_pin, LOW);
-    // Wait for a "ready state"
+  // Write length bytes
+  digitalWrite(slave_pin, HIGH);
+  SPI.transfer(0x05); // Write command
+  SPI.transfer(addr);
+  SPI.transfer(buffer[3]);
+  SPI.transfer(buffer[4]);
+  digitalWrite(slave_pin, LOW);
+  // Wait for a "ready state"
+  delay(5);
+  digitalWrite(slave_pin, HIGH);
+  byte status = 0x00;
+  while(status == 0x00) {
     delay(5);
-    digitalWrite(slave_pin, HIGH);
-    byte status = 0x00;
-    while(status == 0x00) {
-      delay(5);
-      status = SPI.transfer(0x00);
-    }
-    digitalWrite(slave_pin, LOW);
+    status = SPI.transfer(0x00);
   }
+  digitalWrite(slave_pin, LOW);
+  pSer.send(WRITE_ACK_PACKET, 1);
   SPI.endTransaction();
-  reset();
-  blink_led(1000);
-  blink_led(250);
-  blink_led(250);
-  Serial.write("WRIT");
-  Serial.flush();
 }
 
-void eeprom_read() {
+void eeprom_read(unsigned int addr, unsigned int length) {
+  Serial.flush();
   SPI.beginTransaction(spiSettings);
   digitalWrite(slave_pin, HIGH);
   SPI.transfer(0x06); // Write command
-  SPI.transfer(0x00); // Address 0
+  SPI.transfer(addr); // Address 0
 
   // This eeprom throws out a dummy bit on the first byte read.
   byte dangling_byte = SPI.transfer(0x00) << 1;
 
-  // Read 256 bytes
-  for (int i=0; i <= 254; i++){
+  // Read length bytes
+  for (int i=0; i <= length-2; i++){
     byte curbyte = SPI.transfer(0x00);
     Serial.write(curbyte >> 7 | dangling_byte);
     dangling_byte = curbyte << 1;
@@ -91,27 +109,6 @@ void eeprom_read() {
   digitalWrite(slave_pin, LOW);
   SPI.endTransaction();
   Serial.flush();
-  reset();
-}
-
-void buffer_load() {
-  if (eeprom_idx < 255) {
-    delay(1);
-  } else {
-    Serial.write("LOAD");
-    Serial.flush();
-    command = "00";
-    execute_command = false;
-    eeprom_idx = 0;
-  }
-}
-
-void buffer_print() {
-  Serial.write(eeprom_data, 256);
-  Serial.flush();
-  command = "00";
-  execute_command = false;
-  eeprom_idx = 0;
 }
 
 void blink_led(int ms) {
@@ -122,60 +119,6 @@ void blink_led(int ms) {
   delay(half);
 }
 
-void interoperableSerialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    byte inChar = Serial.read();
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
-    if (!execute_command & (char)inChar == '\n') {
-      if (command == "r") {
-        Serial.write("READ");
-        Serial.flush();
-      }
-      if (command == "l") {
-        eeprom_idx = 0;
-      }
-      if (command == "p") {
-        Serial.write("PRNT");
-        Serial.flush();
-      }
-      command += (char)inChar;
-      execute_command = true;
-    } else {
-      if (command == "l\n") {
-        eeprom_data[eeprom_idx] = inChar;
-        eeprom_idx++;
-      } else {
-        // set the command
-        command = (char)inChar;
-      }
-    }
-  }
-}
-
 void loop() {
-  interoperableSerialEvent();
-  if (execute_command) {
-    if (command == "r\n") {
-      eeprom_read();
-    }
-    if (command == "w\n") {
-      eeprom_write();
-    }
-    if (command == "l\n") {
-      buffer_load();
-    }
-    if (command == "p\n") {
-      buffer_print();
-    }
-    if (command == "0\n") {
-      // TODO: More heavy handed reset, like reading everything left in
-      // the serial buffer, and throwing it away, etc.      
-      reset();
-      Serial.write("Reset");
-      Serial.flush();
-      blink_led(2000);
-    }
-  }
+  pSer.update();
 }
