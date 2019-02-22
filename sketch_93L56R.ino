a28202bfbdabaf17e7bdcb71b300dbf4da2c7918
Ryan Geyer <me@ryangeyer.com>
2019-02-21 17:36:14 -0800

    MW writes in chunks delivered by COBS packets. Added I2C read

--- sketch_93L56R.ino
+++ sketch_93L56R.ino
@@ -1,5 +1,6 @@
 #include <PacketSerial.h>
 #include <SPI.h>
+#include <Wire.h>
 
 #if defined(TEENSYDUINO)
   #if defined(__AVR_ATmega32U4__)
@@ -15,8 +16,8 @@
 
 uint8_t RESET_REQ_PACKET[1] = {0};
 uint8_t RESET_ACK_PACKET[1] = {128};
-uint8_t READ_ACK_PACKET[1] = {129};
-uint8_t WRITE_ACK_PACKET[1] = {130};
+uint8_t MW_READ_ACK_PACKET[1] = {129};
+uint8_t MW_WRITE_ACK_PACKET[1] = {130};
 
 const int slave_pin = 10;
 PacketSerial pSer;
@@ -29,24 +30,28 @@ void reset() {
 }
 
 void onSerPacketReceived(const uint8_t* buffer, size_t size) {
+  unsigned int devAddr = 0x50;
   unsigned int addr = 0;
   unsigned int len = 256;
   switch (buffer[0]) {
-    case 0x00:
+    case 0x00: // RESET_REQ_PACKET Command ID
       reset();
       break;
-    case 0x01:
-      if (size >= 3) {
-        addr = (int)(buffer[1] << 8) | (int)(buffer[2]);
-      }
-      if (size >= 5) {
-        len = (int)(buffer[3] << 8) | (int)(buffer[4]);
-      }
-      eeprom_read(addr, len);
-      break;
-    case 0x02:
+    case 0x01: // MW_READ_PACKET Command ID
       addr = (int)(buffer[1] << 8) | (int)(buffer[2]);
-      eeprom_write(addr, buffer);
+      len = (int)(buffer[3] << 8) | (int)(buffer[4]);
+      mw_eeprom_read(addr, len);
+      break;
+    case 0x02: // MW_WRITE_PACKET Command ID
+      addr = (int)(buffer[1] << 8) | (int)(buffer[2]);
+      len = (int)(buffer[3] << 8) | (int)(buffer[4]);
+      mw_eeprom_write(addr, len, buffer);
+      break;
+    case 0x03: // I2C_READ_PACKET Command ID
+      devAddr = buffer[1];
+      addr = (int)(buffer[2] << 8) | (int)(buffer[3]);
+      len = (int)(buffer[4] << 8) | (int)(buffer[5]);
+      i2c_eeprom_read(devAddr, addr, len);
       break;
   }
 }
@@ -57,9 +62,10 @@ void setup() {
   pSer.setPacketHandler(&onSerPacketReceived);
   pinMode(slave_pin, OUTPUT);
   SPI.begin();
+  Wire.begin();
 }
 
-void eeprom_write(unsigned int addr, uint8_t* buffer) {
+void mw_eeprom_write(unsigned int addr, unsigned int len, uint8_t* buffer) {
   // Write enable first!
   SPI.beginTransaction(spiSettings);
   digitalWrite(slave_pin, HIGH);
@@ -68,26 +74,34 @@ void eeprom_write(unsigned int addr, uint8_t* buffer) {
   digitalWrite(slave_pin, LOW);
 
   // Write length bytes
-  digitalWrite(slave_pin, HIGH);
-  SPI.transfer(0x05); // Write command
-  SPI.transfer(addr);
-  SPI.transfer(buffer[3]);
-  SPI.transfer(buffer[4]);
-  digitalWrite(slave_pin, LOW);
-  // Wait for a "ready state"
-  delay(5);
-  digitalWrite(slave_pin, HIGH);
-  byte status = 0x00;
-  while(status == 0x00) {
+  for(int i=0; i < len/2; i++) {
+    byte curAddr = i+addr;
+    byte oneIdx = i*2+5;
+    byte twoIdx = i*2+6;
+    byte one = buffer[oneIdx];
+    byte two = buffer[twoIdx];
+
+    digitalWrite(slave_pin, HIGH);
+    SPI.transfer(0x05); // Write command
+    SPI.transfer(curAddr);
+    SPI.transfer(one);
+    SPI.transfer(two);
+    digitalWrite(slave_pin, LOW);
+    // Wait for a "ready state"
     delay(5);
-    status = SPI.transfer(0x00);
+    digitalWrite(slave_pin, HIGH);
+    byte status = 0x00;
+    while(status == 0x00) {
+      delay(5);
+      status = SPI.transfer(0x00);
+    }
+    digitalWrite(slave_pin, LOW);
   }
-  digitalWrite(slave_pin, LOW);
-  pSer.send(WRITE_ACK_PACKET, 1);
+  pSer.send(MW_WRITE_ACK_PACKET, 1);
   SPI.endTransaction();
 }
 
-void eeprom_read(unsigned int addr, unsigned int length) {
+void mw_eeprom_read(unsigned int addr, unsigned int length) {
   Serial.flush();
   SPI.beginTransaction(spiSettings);
   digitalWrite(slave_pin, HIGH);
@@ -108,6 +122,48 @@ void eeprom_read(unsigned int addr, unsigned int length) {
   Serial.write(last_byte >> 7 | dangling_byte);
   digitalWrite(slave_pin, LOW);
   SPI.endTransaction();
+  Serial.flush();
+}
+
+void i2c_eeprom_read(int devAddr, int addr, int len) {
+  // TODO: Maybe? This is a big heavy handed and inelegant, but it covers
+  // all possible scenarios and stays within the 32byte I2C limit of the
+  // Arduino. This could probably be more elegantly expressed with a single
+  // loop if I spent a few more cycles thinking about it and testing.
+  //
+  // See: https://github.com/arduino/Arduino/issues/5871
+
+  // TODO: This only works with I2C EEPROMs with up to 256 addresses since it
+  // only sends a single address byte in the read request. Need to allow the
+  // write command to indicate the eeprom size, so that we can determine how
+  // many bytes to send for the address in the read request.
+  int loops = len/32;
+  int remainder = len % 32;
+  int i = 0;
+  for (int l=0; l < loops; l++) {
+    Wire.beginTransmission(devAddr);
+    Wire.write(addr+(l*32));
+    Wire.endTransmission();
+    Wire.requestFrom(devAddr,32);
+    for (i=i; i < l*32+32; i++) {
+      if(Wire.available()) {
+        Serial.write(Wire.read());
+      } else {
+        Serial.write(i);
+      }
+    }
+  }
+  Wire.beginTransmission(devAddr);
+  Wire.write(addr+(loops*32));
+  Wire.endTransmission();
+  Wire.requestFrom(devAddr,remainder);
+  for (i=i; i < loops*32+remainder; i++) {
+    if(Wire.available()) {
+      Serial.write(Wire.read());
+    } else {
+      Serial.write(i);
+    }
+  }
   Serial.flush();
 }
 
